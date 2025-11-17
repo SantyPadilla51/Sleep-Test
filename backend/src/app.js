126e6ed70c5df99a26330c20c9c7666cb0d65a85
Santiago Padilla <padillasantiago51@gmail.com>
2025-11-16 23:10:49 -0300

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-// Importar rutas (CommonJS)
-//const adminRoutes = require("./routes/admin.routes");
-//const preguntasRoutes = require("./routes/preguntas.routes");
-//const respuestasRoutes = require("./routes/respuestas.routes");
 const usuariosRoutes = require("./routes/usuarios.routes");
 const testRoutes = require("./routes/test.routes");
 
@@ -16,11 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//app.use("/admin", adminRoutes);
-//app.use("/preguntas", preguntasRoutes);
-//app.use("/respuestas", respuestasRoutes);
 app.use("/usuarios", usuariosRoutes);
-app.use("tests", testRoutes);
+app.use("/tests", testRoutes);
 
 const PORT = process.env.PORT || 3000;
 
